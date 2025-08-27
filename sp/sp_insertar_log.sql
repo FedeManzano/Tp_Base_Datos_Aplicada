e8aea12ca681049d79c9a34198556777d0c77873
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-08-27 02:28:42 -0300

    SP Insertar log terminado la simulación de la bitácora, envuelta en una transacción para no tener problemas de concurrencia en la modificación de la tabla Registro

--- sp/sp_insertar_log.sql
+++ sp/sp_insertar_log.sql
@@ -1,16 +1,39 @@
 USE db_grupo_bases_datos_ap
 
 GO
-CREATE OR ALTER PROCEDURE ddbba.sp_insertar_log
+CREATE OR ALTER PROCEDURE ddbba.sp_Insertar_Log
 @MODULO VARCHAR(10),
 @TEXTO VARCHAR(50)
 AS
 BEGIN
-    IF @MODULO = ''
-        SET @MODULO = 'N/A'
 
-    
+    BEGIN TRANSACTION T_LOG -- Inicio la transacción 
+                            -- No hace definir el nivel de aislamiento
+                            -- ya que al ser una inserción el bloqueo utilizado es EXCLUSIVE
+    BEGIN TRY
+        IF @MODULO IS NULL OR @TEXTO IS NULL 
+            RAISERROR('Los parámetros no pueden ser nulos en la tabla Registro', 20, 1) -- Severidad = 20 Corta la ejecución
+
+        IF @MODULO = ''
+            SET @MODULO = 'N/A'
+        
+        INSERT INTO ddbba.Registro(Modulo, Texto)
+        VALUES 
+        (@MODULO, @TEXTO)
+        COMMIT TRANSACTION
+    END TRY 
+    BEGIN CATCH
+        DECLARE @MJE_ERROR NVARCHAR(200) = '',
+                @SEVERIDAD INT,
+                @ESTADO INT 
+        SELECT @MJE_ERROR = ERROR_MESSAGE(),
+               @SEVERIDAD = ERROR_SEVERITY(),
+               @ESTADO = ERROR_STATE()
+
+        RAISERROR(@MJE_ERROR, @SEVERIDAD, @ESTADO) 
+        ROLLBACK TRANSACTION 
+    END CATCH    
 END 
 
 
---DROP PROCEDURE ddbba.sp_insertar_log
+--DROP PROCEDURE ddbba.sp_Insertar_Log
