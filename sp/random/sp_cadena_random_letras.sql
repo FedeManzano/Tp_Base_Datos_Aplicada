9fc1f0d7c09a6d6d4795b74c8ef730272c4c56b1
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-08-27 01:55:26 -0300

    SP que genera cadenas Random de letras, para las patentes de los vehículos que poseen las personas del dominio del problema

---
+++ sp/random/sp_cadena_random_letras.sql
@@ -0,0 +1,36 @@
+USE db_grupo_bases_datos_ap
+
+GO
+/**
+    Sp que genera una cadena de letras random para resolver el problema de las patentes de los vehículos.
+    Recibe la cantidad de caracteres que tiene que tener la cadena y devuelve a través 
+    del parámetro OUTPUT la cadena solicitada
+
+    EJ
+    DECLARE @RES VARCHAR(10) = ''
+    EXEC ddbba.sp_Cadena_Random_Letras 4,@S_RES = @RES OUTPUT
+    SELECT @RES 
+*/
+CREATE OR ALTER PROCEDURE ddbba.sp_Cadena_Random_Letras
+@CANT_LETRAS INT, 
+@S_RES VARCHAR(MAX) = '' OUTPUT
+AS 
+BEGIN 
+    IF  @CANT_LETRAS IS NULL    OR 
+        @CANT_LETRAS <= 0       OR 
+        @CANT_LETRAS >= 10
+        RETURN 0
+    DECLARE @LETRAS VARCHAR(26) = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    DECLARE @I INT = 0
+    DECLARE @INDEX INT = 0
+
+    WHILE @I < @CANT_LETRAS
+    BEGIN 
+        SET @INDEX = CAST((RAND() * LEN(@LETRAS) + 1 ) AS INT)
+        SET @S_RES = @S_RES + SUBSTRING(@LETRAS, @INDEX, 1)
+        SET @I = @I + 1
+    END 
+    RETURN 1 -- Para hacer las pruebas
+END
+
+
